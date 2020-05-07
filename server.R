a254d73253565539ab3a1e6cc916a5de69d9745a
wildcamping <stuartbarker@Stuarts-MBP.connect>
2020-05-07 08:32:11 +0100

    add notification button if they aren't in Bristol

--- server.R
+++ server.R
@@ -45,6 +45,7 @@ shinyServer(function(input, output) {
             md_cls <- rbind(md_cls, lc)
             } else{
                 md_cls <- lc
+                
             }
             
             #Define marker icon
